ecf883eac8bd2ed73c4350dde4e70ab90bee2aec
Marco Tereso <marco.tereso@gmail.com>
2022-10-03 14:49:09 +0100

    menu opcoes

---
+++ ManipulaFicheiros/src/manipulaficheiros/Ficheiro.java
@@ -0,0 +1,32 @@
+
+
+package manipulaficheiros;
+
+
+class Ficheiro {
+
+    static void criaFicheiro() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void verificaSeExiste() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void escreveNoFinalFicheiro() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void escreveFicheiroNovo() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void mostraConteudoFicheiro() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void mostraListaFicheiros() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
